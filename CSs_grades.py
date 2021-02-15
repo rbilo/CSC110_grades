2c0c065cbb89c5c1310b1f870225714b51919c37
Ruslan Bilotkach <45396843+rbilo@users.noreply.github.com>
2021-02-14 23:49:07 -0700

    Update CSs_grades.py
    made more efficient

--- CSs_grades.py
+++ CSs_grades.py
@@ -9,20 +9,15 @@ short little python program that calculates your CSc110 grades
 
 grades_percentage = [0.45, 0.35, 0.05, 0.1, 0.05]
 grades_name = ["exams", "regular PA's", "small PA's", "video quizzes", "prep problems"]
-grades = []
 
 index = 0
+final = 0
 for i in range(len(grades_name)):
     input_grade = float(input("enter your % grade for " + grades_name[index] + ": "))
-    grades.append(input_grade)
-    index += 1
-    
-index = 0
-final = 0
-for i in range(len(grades)):
-    temp = grades[index] * grades_percentage[index]
+    temp = input_grade * grades_percentage[index]
     final += temp
     index += 1
+    
     
 final = format(final, '0.3f')
 print("\nYour final grade is:", final + "%")
